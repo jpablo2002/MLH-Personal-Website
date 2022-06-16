5fd06cffd8f58297a93f4a5fbda483c5695f2303
Juan Pablo Acosta <90735251+jpablo2002@users.noreply.github.com>
2022-06-16 15:44:54 -0600

    Updated website redeployment script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-tmux kill-server
+tmux kill-server || echo "There was no tmux server to kill"
 
-cd /root/project-boisterous-baboons/
+cd ~/project-boisterous-baboons/
 
 git fetch && git reset origin/main --hard
 
@@ -10,4 +10,4 @@ source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-tmux new -d 'source python3-virtualenv/bin/activate; flask run --host=0.0.0.0'
+tmux new -d -s my-portfolio 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
